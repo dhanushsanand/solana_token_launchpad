315070a9263f2f91d52f0d3f9add0a98df4c0fb1
Dhanush Shivanand <dhanushsanand@gmail.com>
2026-03-11 09:05:47 -0700

    Added routes to handle launches creation and update

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ import { getStatus } from "./utils/status";
 const app = express();
 app.use(express.json());
 
-// TODO: GET /api/health
-// TODO: POST /api/auth/register
-// TODO: POST /api/auth/login
-// TODO: POST /api/launches (with tiers?, vesting?)
-// TODO: GET /api/launches (?page, ?limit, ?status)
-// TODO: GET /api/launches/:id (with computed status)
-// TODO: PUT /api/launches/:id
+// TODO: GET /api/health -- done
+// TODO: POST /api/auth/register -- done
+// TODO: POST /api/auth/login -- done
+// TODO: POST /api/launches (with tiers?, vesting?) -- done
+// TODO: GET /api/launches (?page, ?limit, ?status) -- done
+// TODO: GET /api/launches/:id (with computed status) -- done
+// TODO: PUT /api/launches/:id -- done
 // TODO: POST /api/launches/:id/whitelist
 // TODO: GET /api/launches/:id/whitelist
 // TODO: DELETE /api/launches/:id/whitelist/:address
@@ -109,11 +109,69 @@ app.post("/api/launches", authMiddleware, async (req, res)=>{
   });
 })
 
-app.get("/api/launches", (req, res)=>{
+app.get("/api/launches", async (req, res)=>{
   const page = parseInt((req.query.page as string) || "1");
-  const limit = parseInt((req.query.limit as string) || "1");
+  const limit = parseInt((req.query.limit as string) || "10");
   const status = req.query.status;
+  const launches = await prisma.launch.findMany({
+    take:limit,
+    skip:(page-1) * limit,
+    include:{tiers:true, vesting:true, purchases:true}
+  });
+  const launchesWithStatus = launches.map((launch:any) => {
+    const totalPurchased = launch.purchases.reduce((sum:number, p:any) => sum + p.amount, 0);
+    return {...launch, status:getStatus(launch, totalPurchased)};
+  });
+  const totalLaunches = await prisma.launch.count();
+  const filtered = status? launchesWithStatus.filter(launch => launch.status === status) : launchesWithStatus;
+  return res.status(200).json({
+    launches:filtered,
+    total:totalLaunches,
+    page,
+    limit
+  });
 })
+
+app.get("/api/launches/:id", async (req, res)=>{
+  const launchId = parseInt(req.params.id);
+  const launch = await prisma.launch.findUnique({
+    where:{id:launchId},
+    include:{tiers:true, vesting:true,purchases:true}
+  });
+  if(!launch){
+    return res.status(404).json({
+      error:"Launch Not found"
+    });
+  }
+  const totalPurchased = launch.purchases.reduce((sum:number, p:any) => sum + p.amount, 0);
+  return res.status(200).json({
+    ...launch, status:getStatus(launch, totalPurchased)
+  });
+})
+
+app.put("/api/launches/:id", authMiddleware, async (req, res) => {
+  const launchId = parseInt(req.params.id);
+  const userId = (req as any).userId;
+  const launch = await prisma.launch.findUnique({
+    where:{id:launchId},
+    include:{purchases:true}
+  });
+  if(!launch) return res.status(404).json({error:"Launch Not found"});
+  if(launch.creatorId !== userId) return res.status(403).json({error:"Unauthorized access"});
+  try {
+    const updatedLaunch = await prisma.launch.update({
+    where:{id:launchId},
+    data:req.body,
+    include:{purchases:true}
+  })
+  const totalPurchased = updatedLaunch.purchases.reduce((sum:any, p:any)=> sum + p.amount,0);
+  return res.status(200).json({...updatedLaunch, status:getStatus(updatedLaunch, totalPurchased)});
+  } catch (error) {
+    return res.status(500).json({error:"Unexpected error"});
+  }
+})
+
+app.post
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
